29275eb29607f8b700ec51ac8333539547757077
santhosh kumar <santhoshkumar3479@gmail.com>
2024-12-28 16:29:44 +0530

    Initial Commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by iPhone Apps Nano Nino on 28/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LoginAppTests/LoginAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAppTests.swift
+//  LoginAppTests
+//
+//  Created by iPhone Apps Nano Nino on 28/12/24.
+//
+
+import Testing
+@testable import LoginApp
+
+struct LoginAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
